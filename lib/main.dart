c9ffc9d13a79cad4c21530c6c9d9b074f4939cdd
Odilbek <odilbekjuraev07@gmail.com>
2023-02-01 14:09:21 +0500

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'ui/counter_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rx Dart',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+    return  GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      // themeMode: ThemeMode.,
+      home: CounterPage(),
     );
   }
+}
+
+class AppTheme{
+  static ThemeData darkTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.black
+  );
+
+  static ThemeData lightTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.white
+  );
 }
---
+++ lib/ui/login_page.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:rx_dart/bloc/login_cubit.dart';
+import 'package:rx_dart/custom_widgets/custom_plain_button.dart';
+import 'package:rx_dart/custom_widgets/custom_text_field.dart';
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final LoginScreenBloc _loginScreenBloc = LoginScreenBloc();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Validation with BloC'),
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Login In',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      StreamBuilder(
+                          stream: _loginScreenBloc.userNameStream,
+                          builder: (context, snapshot) {
+                            return Column(
+                              children: [
+                                CustomTextField(
+                                  onChange: (text) {
+                                    _loginScreenBloc.updateUserName(text);
+                                  },
+                                  labelText: 'Username',
+                                  textInputType: TextInputType.emailAddress,
+                                ),
+                                if (snapshot.hasError)
+                                  Text(snapshot.error.toString()),
+                              ],
+                            );
+                          }),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      StreamBuilder(
+                          stream: _loginScreenBloc.passwordStream,
+                          builder: (context, snapshot) {
+                            return Column(
+                              children: [
+                                CustomTextField(
+                                  onChange: (text) {
+                                    _loginScreenBloc.updatePassword(text);
+                                  },
+                                  labelText: 'Password',
+                                  textInputType: TextInputType.text,
+                                ),
+                                if (snapshot.hasError)
+                                  Text(snapshot.error.toString()),
+                              ],
+                            );
+                          }),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  )),
+              ElevatedButton(
+                onPressed: () {
+                  _loginScreenBloc.increment();
+                },
+                child: const Text("+"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _loginScreenBloc.decrement();
+                },
+                child: StreamBuilder(
+                  stream: _loginScreenBloc.getIntegers,
+                  builder: (context, snapshot) {
+                    return Text(snapshot.data.toString());
+                  },
+                ),
+              ),
+              StreamBuilder(
+                stream: _loginScreenBloc.validateForm,
+                builder: (context, snapshot) {
+                  return CustomPlainButton(
+                    isEnabled: snapshot.hasData,
+                    btnColor: snapshot.hasData ? Colors.red : Colors.grey,
+                    height: 67,
+                    onTap: snapshot.hasData
+                        ? () {
+                      print('Login Button Pressed');
+                    }
+                        : null,
+                    label: 'Log in',
+                    lblColor: Colors.white,
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
